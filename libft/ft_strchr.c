878c6087528939a909095bb9b9e436f0c5c8c04c
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2022-08-03 18:24:12 +0900

    Add files via upload

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: cyun <cyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:26:37 by cyun              #+#    #+#             */
-/*   Updated: 2022/07/24 17:11:28 by cyun             ###   ########seoul.kr  */
+/*   Updated: 2022/08/03 17:54:49 by cyun             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strchr(const char *s, int c)
 	len = ft_strlen(s);
 	while (i <= len)
 	{
-		if (s[i] == (unsigned char)c)
+		if (s[i] == (char)c)
 			return ((char *)(s + i));
 		i++;
 	}
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: cyun <cyun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:32:42 by cyun              #+#    #+#             */
-/*   Updated: 2022/07/22 15:57:58 by cyun             ###   ########seoul.kr  */
+/*   Updated: 2022/08/03 18:17:37 by cyun             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
@@ -20,23 +20,14 @@ char	*ft_strtrim(char const *s1, char const *s2)
 
 	if (!s1)
 		return (NULL);
-	if (!s2)
-		return (NULL);
+	if (!set)
+		return ((char *)s1);
 	start = 0;
-	end = ft_strlen(s1);
-	while (s1[start] && ft_strchr(s2, s1[start]))
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (s1[end - 1] && ft_strchr(s2, s1[end - 1]))
-	{
-		if (end - 1 < 1)
-			break ;
+	end = ft_strlen(s1);
+	while (ft_strchr(set, s1[end - 1]) && end > start)
 		end--;
-	}
-	if (start > end)
-		return (ft_strdup(""));
-	res = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (!res)
-		return (NULL);
-	ft_strlcpy(res, s1 + start, end - start + 1);
+	res = ft_substr(s1, start, (end - start));
 	return (res);
 }
